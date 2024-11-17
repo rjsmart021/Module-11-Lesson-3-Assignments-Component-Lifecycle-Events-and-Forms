c37c0f655c6ed61b151b928e0e2aa28c0d0adc3c
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-16 18:28:50 -0600

    Update Search.js

--- Search.js
+++ Search.js
@@ -1,19 +1,4 @@
-yarn create vite
-yarn add sass md5
-VITE_PUBLIC_KEY = "YOUR_PUBLIC_KEY"
-VITE_PRIVATE_KEY = "YOUR_PRIVATE_KEY"
-// App.js
 
-import "./App.css"
-import Search from "./components/Search"
-
-function App() {
-  return (
-    <div className="App">
-      <Search />
-    </div>
-  )
-}
 import "../styles/Search.scss"
 import { useState } from "react"
 import md5 from "md5"
